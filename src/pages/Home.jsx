f4a62c9e88fae2e867d55ca5860d578116d731da
Леонтьев Александр <LeontyevAV@mail.ru>
2023-05-24 12:11:15 +0300

    Всё

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,54 @@ import { getCatigories } from '../api';
 import { Preloader } from '../components/Preloader';
 import { CotegoryList } from '../components/CategoryList';
 import { Search } from '../components/Search';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Home() {
 	const [catalog, setCatalog] = useState([]);
 	const [filteredCatalog, setFilteredCatalog] = useState([]);
-    
-	useEffect(() => {
-		getCatigories().then(data => {
-			setCatalog(data.categories);
-			setFilteredCatalog(data.categories);
-		});
-	}, []);
 
-    return (
-        <>
-            <Search cb = {}/>
-            {!catalog.length ? ( <Preloader />) : (<CotegoryList catalog={filteredCatalog}/>)}
-        </>
-    )
+	console.log(useLocation());
+	//console.log(useNavigate());
+	//const {pathname, search} = useLocation();
+	const { pathname, search } = useLocation();
+	const navigate = useNavigate();
+	const pushPath = (str) => {
+		navigate(pathname + `?search=${str}`);
+	};
+
+	const handleSearch = (str) => {
+		setFilteredCatalog(
+			catalog.filter((item) =>
+				item.strCategory.toLowerCase().includes(str.toLowerCase())
+			)
+		);
+		pushPath(str);
+	};
+
+	useEffect(() => {
+		const str = search.split('=')[1].toLowerCase();
+		getCatigories().then((data) => {
+			setCatalog(data.categories);
+			setFilteredCatalog(
+				search
+					? data.categories.filter((item) =>
+							item.strCategory.toLowerCase().includes(str)
+					  )
+					: data.categories
+			);
+		});
+	}, [search]);
+
+	return (
+		<>
+			<Search cb={handleSearch} />
+			{!catalog.length ? (
+				<Preloader />
+			) : (
+				<CotegoryList catalog={filteredCatalog} />
+			)}
+		</>
+	);
 }
 
 export { Home };
